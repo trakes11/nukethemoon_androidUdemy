daf81d0845b393774b02876e7f8bd2bf8040c232
trakes11 <tom.rakes@gmail.com>
2017-04-14 07:04:04 -0700

    Hello World

---
+++ app/src/main/java/com/thomas/blowupthemoon/ViewController.java
@@ -0,0 +1,12 @@
+package com.thomas.blowupthemoon;
+
+/**
+ * Created by Thomas on 4/14/2017.
+ */
+
+public class ViewController {
+
+    public void print(){
+        System.out.println("Hello World");
+    }
+}
