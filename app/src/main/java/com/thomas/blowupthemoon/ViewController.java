cb3947395bd84cdb7655c604230ea8f5282c120c
trakes11 <tom.rakes@gmail.com>
2017-04-14 21:31:03 -0700

    W
    To Master...directly

--- app/src/main/java/com/thomas/blowupthemoon/ViewController.java
+++ app/src/main/java/com/thomas/blowupthemoon/ViewController.java
@@ -7,6 +7,6 @@ package com.thomas.blowupthemoon;
 public class ViewController {
 
     public void print(){
-        System.out.println("Hello World");
+        System.out.println("Hello World. Let's keep the moon");
     }
 }
